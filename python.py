c2fe5629da7c4a85faa293f3a1d2bd0c7abc6f56
Cristian Dinu <cristian69dinu@gmail.com>
2026-09-09 18:03:44 +0300

    added matplotlib

--- python.py
+++ python.py
@@ -1,5 +1,10 @@
+# modificare facuta de pe MAC
+
 import pandas as pd
 import numpy as np
 print("JJJJJ")
 
-# modificare facuta de pe MAC
+# modificare facuta de pe main PC
+
+import matplotlib.pyplot
+plt.bar(x)
